78c83ee6855da45412be49ea503dac8bfe029482
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-04-27 14:24:48 +0800

    Created the game function, fixed some errors in the first function, added lines below to execute the function

--- p1_extracting_odd_and_even_numbers/main.py
+++ p1_extracting_odd_and_even_numbers/main.py
@@ -13,7 +13,7 @@ class OddEvenDetector:
 		print("Data loaded successfully!")
 
 		while True:
-			choice = input("/nDo you want to play a game? Y/N").upper()
+			choice = input("\nDo you want to play a game? Y/N: ").upper()
 			if choice == "Y":
 				print("Game starting...")
 				self.odd_even_game(read_contents)
@@ -26,4 +26,35 @@ class OddEvenDetector:
 				print("Invalid input. Please try again with [Y/N]").upper()
 
 	def odd_even_game(self, read_content):
-		
+		even_nums = []
+		odd_nums = []
+		score = 0
+
+		print("---Welcome to Odd Even Game!---")
+		print('Instructions: type "E" if the number displayed is even, type "O" if it is odd.')
+
+		for line in read_content:
+			number = line.strip()
+			if number != "":
+				number = int(number)
+				ans = input(f"Is {number} even or odd?[E/O]: ").upper()
+				if ans == "E":
+					if number % 2 == 0:
+						score +=1
+						even_nums.append(number)
+						print("You are correct!")
+					else:
+						print("Engk! You are wrong")
+				elif ans == "O":
+					if number % 2 != 0:
+						score += 1
+						odd_nums.append(number)
+						print("You are correct!")
+					else:
+						print("Engk! You are wrong.")
+				else:
+					print("Invalid input! You are automatically wrong, try again!")
+
+
+files = OddEvenDetector("numbers.txt", "even.txt", "odd.txt")
+files.read_file()
