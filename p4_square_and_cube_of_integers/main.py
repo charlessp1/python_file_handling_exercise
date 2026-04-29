55ab84fa99c5a6ec80cab5c32a7a5af619530db6
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-04-29 10:18:13 +0800

    Successfullu added function to get the cubed of an integer. Used the same method in the ealier function that calculates for square. Now making room for debugging.

--- p4_square_and_cube_of_integers/main.py
+++ p4_square_and_cube_of_integers/main.py
@@ -10,7 +10,7 @@ class IntegersSquareCube:
 		with open(self.source_file, "r") as source_file:
 			source_content = source_file.readlines()
 
-		print("Data loaded successfully! Calculating...")
+		print("Data loaded successfully! Calculating square of integers...")
 
 		squared_numbers = []
 		for line in source_content:
@@ -21,13 +21,33 @@ class IntegersSquareCube:
 				format = str(num) + " ** 2 = " + str(squared)
 				squared_numbers.append(format)
 
-		print("Saving answers...")
+		print("Calculation complete! Saving file...")
 
 		with open(self.double_file, "w") as double_file:
 			for answer in squared_numbers:
 				double_file.write(answer + "\n")
 
 		print(f"Saving complete! File name: {self.double_file}")
+
+	def cube_of_integers(self, source_content):
+		print("Calculating cube of intehers...")
+
+		cubed_numbers = []
+		for line in source_content:
+			clean_line = line.strip()
+			if clean_line != "":
+				num = int(clean_line)
+				cubed = num ** 2
+				format = str(num) + " ** 2 = " + str(cubed)
+				cubed_numbers.append(format)
+
+		print("Calculation complete! Saving file...")
+
+		with open(self.triple_file, "w") as triple_file:
+			for answer in cubed_numbers:
+				triple_file.write(answer + "\n")
+
+		print(f"Saving complete! File name: {self.triple_file}")
 
 calculate = IntegersSquareCube("integers.txt", "double.txt", "triple.txt")
 calculate.square_of_integers()
