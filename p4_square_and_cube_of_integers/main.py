88f880328ebe8d90c282dc262951833f8d5129f7
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-04-29 10:03:29 +0800

    Completed function for squaring integers from source file. I created a format, added them to a list, then had that list written to the double.txt file line by line, thus, now showing answers to each integer in the file.

--- p4_square_and_cube_of_integers/main.py
+++ p4_square_and_cube_of_integers/main.py
@@ -5,7 +5,29 @@ class IntegersSquareCube:
 		self.triple_file = triple_file
 
 	def square_of_integers(self):
-		print(f"Calculating square of integers in file: {self.source_file}")
+		print(f"Initializing reading process for file: {self.source_file}")
 
 		with open(self.source_file, "r") as source_file:
-			
+			source_content = source_file.readlines()
+
+		print("Data loaded successfully! Calculating...")
+
+		squared_numbers = []
+		for line in source_content:
+			clean_line = line.strip()
+			if clean_line != "":
+				num = int(clean_line)
+				squared = num ** 2
+				format = str(num) + " ** 2 = " + str(squared)
+				squared_numbers.append(format)
+
+		print("Saving answers...")
+
+		with open(self.double_file, "w") as double_file:
+			for answer in squared_numbers:
+				double_file.write(answer + "\n")
+
+		print(f"Saving complete! File name: {self.double_file}")
+
+calculate = IntegersSquareCube("integers.txt", "double.txt", "triple.txt")
+calculate.square_of_integers()
